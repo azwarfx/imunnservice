f34a3e2ef8975249a0408c03727840ab606c18d8
azwarazuhri <data.azwarazuhri@gmail.com>
2018-09-20 14:35:29 +0700

    komit pertama

---
+++ src/main/java/com/azwarazuhri/imunn/imunnservice/ImunnserviceApplication.java
@@ -0,0 +1,12 @@
+package com.azwarazuhri.imunn.imunnservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImunnserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImunnserviceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/azwarazuhri/imunn/imunnservice/ImunnserviceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.azwarazuhri.imunn.imunnservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ImunnserviceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
